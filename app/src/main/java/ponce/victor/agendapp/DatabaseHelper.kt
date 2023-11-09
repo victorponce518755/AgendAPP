f35360c2cbb19be09d2985f70ab67dcd310d5e3c
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-11-09 17:55:45 -0600

    Initial commit

---
+++ app/src/main/java/ponce/victor/agendapp/DatabaseHelper.kt
@@ -0,0 +1,73 @@
+package ponce.victor.agendapp
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.sql.Time
+import java.sql.Date
+
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASENAME, null, DATABASEVERSION)
+{
+
+    companion object{
+        private val DATABASENAME = "Agenda.db"
+        private val DATABASEVERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_TABLE_QUERY = "CREATE TABLE eventos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, fecha date, hora time, descripcion TEXT, lugar TEXT)"
+        db?.execSQL(CREATE_TABLE_QUERY)
+
+    }
+
+    override  fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS eventos ")
+        onCreate(db)
+    }
+
+
+    // si no nos acepta el tipo de dato date y time, hay que hacer modificaciones aqui
+    fun addEvent(nombre: String, fecha: Date, hora: Time , descripcion: String, lugar:String ):Boolean{
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put("nombre", nombre)
+        values.put("fecha", fecha.toString())
+        values.put("hora", hora.toString())
+        values.put("descripcion", descripcion)
+        values.put("lugar", lugar)
+        val result = db.insert("eventos", null, values)
+
+        db.close()
+        return result != -1L
+    }
+
+    fun getAllEvents() : Cursor {
+        val db = this.writableDatabase
+        return db.rawQuery("SELECT * FROM eventos", null)
+    }
+
+    fun updateEvent(id: String, nombre: String, fecha: Date, hora: Time , descripcion: String, lugar:String ):Boolean{
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put("nombre", nombre)
+        values.put("fecha", fecha.toString())
+        values.put("hora", hora.toString())
+        values.put("descripcion", descripcion)
+        values.put("lugar", lugar)
+        val result = db.update("eventos", values, "id=?", arrayOf(id.toString()))
+
+        db.close()
+        return result > 0
+    }
+
+    fun deleteEvent(id: String):Boolean{
+        val db = this.writableDatabase
+        val result = db.delete("eventos", "id=?", arrayOf(id.toString()))
+
+        db.close()
+        return result > 0
+    }
+
+}

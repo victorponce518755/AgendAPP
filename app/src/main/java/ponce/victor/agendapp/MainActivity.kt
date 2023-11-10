c26f7aa4910b272a6812ecbc6afb8306d7333af6
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-11-09 18:38:41 -0600

    Front falta por arreglarse y mostrar los datos en el listview

--- app/src/main/java/ponce/victor/agendapp/MainActivity.kt
+++ app/src/main/java/ponce/victor/agendapp/MainActivity.kt
@@ -2,10 +2,25 @@ package ponce.victor.agendapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var databaseHelper: DatabaseHelper
+    private lateinit var txtResults: TextView
+    private  lateinit var listView: ArrayAdapter<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        databaseHelper = DatabaseHelper(this)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+
+
     }
 }
